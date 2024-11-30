eb76146e9587f600240be087b78641ba0dd3f21e
Swordsteel <swordsteel@gmail.com>
2024-11-30 14:45:16 +0100

    update dependence for catalog version 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,25 +7,25 @@ plugins {
 }
 
 dependencies {
-    implementation(hlaeja.com.fasterxml.jackson.module.kotlin)
+    implementation(hlaeja.fasterxml.jackson.module.kotlin)
     implementation(hlaeja.jjwt.api)
     implementation(hlaeja.kotlin.logging)
     implementation(hlaeja.kotlin.reflect)
     implementation(hlaeja.kotlinx.coroutines)
-    implementation(hlaeja.ltd.hlaeja.library.common.messages)
-    implementation(hlaeja.org.springframework.springboot.actuator.starter)
-    implementation(hlaeja.org.springframework.springboot.webflux.starter)
+    implementation(hlaeja.library.hlaeja.common.messages)
+    implementation(hlaeja.springboot.starter.actuator)
+    implementation(hlaeja.springboot.starter.webflux)
 
     runtimeOnly(hlaeja.jjwt.impl)
     runtimeOnly(hlaeja.jjwt.jackson)
 
-    testImplementation(hlaeja.io.mockk)
-    testImplementation(hlaeja.io.projectreactor.reactor.test)
     testImplementation(hlaeja.kotlin.test.junit5)
     testImplementation(hlaeja.kotlinx.coroutines.test)
-    testImplementation(hlaeja.org.springframework.springboot.test.starter)
+    testImplementation(hlaeja.mockk)
+    testImplementation(hlaeja.projectreactor.reactor.test)
+    testImplementation(hlaeja.springboot.starter.test)
 
-    testRuntimeOnly(hlaeja.org.junit.platform.launcher)
+    testRuntimeOnly(hlaeja.junit.platform.launcher)
 }
 
 group = "ltd.hlaeja"
