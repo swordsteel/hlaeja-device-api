47bfa9802c015886f438494827f3fc9b993f1972
Swordsteel <swordsteel@gmail.com>
2024-11-30 20:40:51 +0100

    add Mapper.kt with Node.Response.toDeviceResponse

---
+++ src/main/kotlin/ltd/hlaeja/util/Mapper.kt
@@ -0,0 +1,10 @@
+package ltd.hlaeja.util
+
+import ltd.hlaeja.library.deviceConfiguration.Node
+
+fun Node.Response.toDeviceResponse(): Map<String, String> {
+    return mapOf(
+        "version" to timestamp.toEpochSecond().toString(),
+        "data" to configuration
+    )
+}
